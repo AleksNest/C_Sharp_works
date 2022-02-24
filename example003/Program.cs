3570f31d97e9a6ba070dea16b2f1a5905762fa0b
Alexx <al_nesteov@inbox.ru>
2022-02-24 04:07:52 +0300

    example003

---
+++ example003/Program.cs
@@ -0,0 +1,5 @@
+﻿int NumberA = new Random ().Next (1, 10);
+Console.WriteLine (NumberA);
+int NumberB = new Random ().Next (1, 10);
+Console.WriteLine(NumberB);
+Console.WriteLine(NumberA + NumberB); 
