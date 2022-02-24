1bc58a5deff483d3d7903f99ad5fffa4f83ba4c2
Alexx <al_nesteov@inbox.ru>
2022-02-24 05:20:06 +0300

    example006

---
+++ example006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int xa = 40, ya = 1, 
+    xb = 1, yb = 30, 
+    xc = 80, yc = 30;
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine ("+");
+Console.SetCursorPosition (xb, yb);
+//Console.WriteLine ("+");
+//Console.SetCursorPosition (xc, yc);
+//Console.WriteLine ("+");
+int x = xa, y = xy;
+ int count = 0;
+
+ while(count < 10)
+ 
