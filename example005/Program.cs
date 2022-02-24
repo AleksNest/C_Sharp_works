bade1847d287c6a7bc094e8230f58f3645399411
Alexx <al_nesteov@inbox.ru>
2022-02-24 04:40:51 +0300

    example005

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿int x1 = 2;
+int x2 = 5;
+int x3 = 10;
+
+int Max = x1;
+if (x1 > Max) Max = x1;
+if (x2 > Max) Max = x2;
+if (x3 > Max) Max = x3;
+
+Console.Write("максимум ="); 
+Console.WriteLine(Max); 
